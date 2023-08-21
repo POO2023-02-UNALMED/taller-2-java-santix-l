57dd434d9098387c08431af07dfb7a333e0885c6
santix-l <137665289+santix-l@users.noreply.github.com>
2023-08-20 22:18:50 -0500

    prueba

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Auto {
+	
+	String modelo;
+	int precio;
+	//Array[Asiento] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int cantidadAsientos() {
+		
+	}
+	
+	String verificarIntegridad() {
+		
+	}
+
+}
