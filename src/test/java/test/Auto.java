b1d524752fb3c5fc1a3b3d0c9e1cfc783dbf69bd
santix-l <137665289+santix-l@users.noreply.github.com>
2023-08-21 18:49:53 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -4,7 +4,7 @@ public class Auto {
 	
 	String modelo;
 	int precio;
-	//Array[Asiento] asientos;
+	Asiento[] asientos;
 	String marca;
 	Motor motor;
 	int registro;
@@ -12,9 +12,44 @@ public class Auto {
 	
 	int cantidadAsientos() {
 		
+		int nAsientos = 0;
+		
+		for (int i = 0; i < asientos.length; i++) {
+			
+			if (asientos[i] != null) {
+			
+				nAsientos++;
+			
+			}
+		}
+		
+		return nAsientos;
 	}
 	
 	String verificarIntegridad() {
+		
+		if(registro == motor.registro) {
+			
+			for (int i = 0; i < asientos.length; i++) {
+				
+				if(asientos[i] != null) {
+					
+					if (asientos[i].registro != registro) {
+						
+						return "Las piezas no son originales";
+					}
+					
+				}
+			}
+		
+			return "Auto original";
+		}
+		
+		else {
+			
+			return "Las piezas no son originales";
+		
+		}
 		
 	}
 
